3987e99e974b072dfe2bc6a19ad80be38f2cbfda
Akim <akim.soula@linkt.fr>
2020-06-11 16:33:13 +0200

    Récuperation données milou et insertion base

--- webserveur.py
+++ webserveur.py
@@ -1,5 +1,12 @@
 from flask import Flask, render_template, request
+from pymongo import MongoClient
+import datetime
+import pprint
 app = Flask(__name__)
+client = MongoClient("mongodb+srv://jeanM:iWjYfAp7COoLJS2Q@cesitest-xb3jc.gcp.mongodb.net/machine_learning?retryWrites=true&w=majority")
+
+db = client['machine_learning']
+collection = db['results']
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -9,7 +16,30 @@ def index():
 def upload():
     try:
         imagefile = request.files.get('avatar', '')
-        return("correctement upload")
+        #TRAITEMENT DE MILOUX ANTOINE
+        datademiloux = "humain"
+        # # # # # # # # # # # # # # # #
+        #ENVOI DES DATA A MANGODB
+        currentDateTime = datetime.datetime.now()
+      
+        result = {
+            "result": datademiloux,
+            "date": currentDateTime.strftime("%A")
+        }
+        collection.insert_one(result)
+        return (datademiloux)
+        
     except Exception as err:
         print(err)
-        return ("Image non uploadé")    
+        return ("Image non uploadé")
+
+@app.route('/results',methods=['POST','GET'])
+def result():
+    collectionData = collection.find() #retourne un array de result
+    print(collectionData)
+    return ('ok')
+
+
+
+
+    
